731c1b45a6482df8c2a5734ad7fee2b082f342e8
saikhmirsat <saikh.mirsat.786@gmail.com>
2023-06-25 22:20:30 +0530

    add backend folder

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const { connection } = require('./config/db')
+const { userRoute } = require('./routes/User.routes')
+
+const app = express()
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Welcome")
+})
+
+app.use("/users", userRoute)
+
+app.listen(8080, async () => {
+    try {
+        await connection
+        console.log("DB connected")
+    } catch (err) {
+        console.log("DB not connected")
+    }
+    console.log("Server porting at 8080")
+})
---
+++ routes/User.routes.js
@@ -0,0 +1,110 @@
+const express = require('express')
+const { userModel } = require('../model/User.model')
+const jwt = require("jsonwebtoken")
+const bcrypt = require('bcrypt');
+
+
+
+const userRoute = express.Router()
+
+userRoute.get('/user', (req, res) => {
+    res.send("User DB")
+})
+
+userRoute.post('/register', async (req, res) => {
+    const { name, email, pass } = req.body
+
+    try {
+        const user = await userModel.find({ email })
+        if (user.length > 0) {
+            res.send({ "msg": "Already have an account please login" })
+        } else {
+            bcrypt.hash(pass, 9, async (err, hash) => {
+                if (err) {
+                    res.send("Something went wrong")
+                } else {
+                    const user = new userModel({ name, email, pass: hash })
+                    await user.save()
+                    res.send({ "msg": "new user has been register", "success": true })
+                }
+            });
+        }
+
+    } catch (err) {
+        console.log(err)
+        res.send({ "msg": "Can't register", "success": false })
+    }
+})
+
+userRoute.post('/login', async (req, res) => {
+    const { email, pass } = req.body
+    try {
+        const user = await userModel.find({ email })
+        if (user.length > 0) {
+            bcrypt.compare(pass, user[0].pass, (err, result) => {
+                if (result) {
+                    const token = jwt.sign({ userID: user[0]._id }, "manyavar")
+                    res.send({ "msg": "Login sucessful", "success": true, token, user })
+                } else {
+                    res.send({ "msg": "Wrong crediential", "success": false })
+                }
+            });
+        } else {
+            res.send({ "msg": "Wrong crediential", "success": false })
+        }
+    } catch (err) {
+        res.send({ "msg": "Something Wrong", "success": false })
+    }
+})
+
+userRoute.get('/', async (req, res) => {
+    try {
+        const allUsers = await userModel.find()
+        res.send(allUsers)
+        console.log(allUsers)
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+userRoute.get('/:_id', async (req, res) => {
+    try {
+        const user = await userModel.find({ _id: req.params })
+        if (user == "") {
+            res.send("This is person not available")
+        } else {
+            res.send(user)
+        }
+        console.log(user)
+
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+userRoute.delete('/delete/:_id', async (req, res) => {
+    try {
+        let _id = req.params._id
+        await userModel.findByIdAndDelete({ _id })
+        res.send({ "msg": "User has been delete", "success": true })
+    } catch (err) {
+        res.send({ "msg": "User has not been delete", "success": false })
+        console.log(err)
+    }
+})
+
+userRoute.patch('/edit/:_id', async (req, res) => {
+    try {
+        let payload = req.body
+        let _id = req.params._id
+        await userModel.findByIdAndUpdate({ _id }, payload)
+        res.send({ "msg": "Updated user", "success": true })
+    } catch (err) {
+        res.send({ "msg": "Updated has not been updated", "success": false })
+        console.log(err)
+    }
+})
+
+module.exports = {
+    userRoute
+}
